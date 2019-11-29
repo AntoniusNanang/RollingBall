13406949a00230e2187e0dca2f34e75a0fb1cfe3
aoitomo1031 <nkcgta.yutaro.aoki@gmail.com>
2019-11-29 17:44:46 +0900

    unko

---
+++ New Unity Project/Assets/HitEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffect : MonoBehaviour
+{
+
+    public GameObject particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "")
+        {
+            Instantiate(particle, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ New Unity Project/Assets/ItemEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemEffect : MonoBehaviour
+{
+
+    public GameObject particle;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "")
+        {
+            Instantiate(particle, transform.position, transform.rotation);
+        }
+    }
+
+}
